2e1b26f988c726cdf22ac9c0653772ec20d02cb5
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2024-01-15 22:10:10 +0000

    Added some comments.

--- random_number.py
+++ random_number.py
@@ -2,7 +2,7 @@ import random
 
 def generate_random_numbers():
     # Step 1: Generate a list of numbers from 1 to 100
-    numbers = list(range(1, 221))
+    numbers = list(range(1, 36)) # + 1 above maximum number
 
     # Step 2: Shuffle the list to get a random order
     random.shuffle(numbers)
